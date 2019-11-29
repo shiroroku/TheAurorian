d510c6efd013aac1d4b59b77d73ee0ec8d32fb37
Elise <elseytd@gmail.com>
2019-11-29 14:08:12 -0800

    Changed to beta

--- TheAurorian/src/main/java/com/elseytd/theaurorian/TAMod.java
+++ TheAurorian/src/main/java/com/elseytd/theaurorian/TAMod.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class TAMod {
 	public static final String MODID = "theaurorian";
 	public static final String NAME = "The Aurorian";
-	public static final String VERSION = "1.12.2-Alpha";
+	public static final String VERSION = "1.12.2-Beta";
 	public static final String VERSION_MINECRAFT = "[1.12.2]";
 	public static final String VERSION_FORGE = "before:tconstruct;required-after:forge@[14.23.5.2847,)";
 
