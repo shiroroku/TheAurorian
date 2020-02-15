72d9b7bcbf5ce399cd569e0f3e87744730a0288e
Elise <elseytd@gmail.com>
2020-02-14 22:26:10 -0800

    Fixed plant generation code
    Made so we ask the plant if it can be there instead of the soil

--- TheAurorian/src/main/java/com/elseytd/theaurorian/World/Feature/TAWorldGenerator_Plant.java
+++ TheAurorian/src/main/java/com/elseytd/theaurorian/World/Feature/TAWorldGenerator_Plant.java
@@ -2,9 +2,9 @@ package com.elseytd.theaurorian.World.Feature;
 
 import java.util.Random;
 
-import com.elseytd.theaurorian.TABlocks;
 import com.elseytd.theaurorian.TAUtil;
 
+import net.minecraft.block.BlockBush;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -25,13 +25,18 @@ public class TAWorldGenerator_Plant extends WorldGenerator {
 		for (IBlockState iblockstate = worldIn.getBlockState(position); (iblockstate.getBlock().isAir(iblockstate, worldIn, position) || iblockstate.getBlock().isLeaves(iblockstate, worldIn, position)) && position.getY() > 0; iblockstate = worldIn.getBlockState(position)) {
 			position = position.down();
 		}
-		for (int i = 0; i < 128; ++i) {
-			BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
+		if (plantState.getBlock() instanceof BlockBush) {
+			BlockBush blk = (BlockBush) plantState.getBlock();
+			for (int i = 0; i < 128; ++i) {
+				BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
 
-			if (worldIn.isAirBlock(blockpos) && TABlocks.silkberryplant.canBlockStay(worldIn, blockpos, this.plantState) && TAUtil.randomChanceOf(plantDensity)) {
-				worldIn.setBlockState(blockpos, this.plantState, 2);
+				if (worldIn.isAirBlock(blockpos) && blk.canBlockStay(worldIn, blockpos, worldIn.getBlockState(blockpos.down())) && TAUtil.randomChanceOf(plantDensity)) {
+					worldIn.setBlockState(blockpos, this.plantState, 2);
+				}
 			}
+			return true;
+		} else {
+			return false;
 		}
-		return true;
 	}
 }
